3ce020ee166ea14e7add4039b784bc65083f1611
hardiksachan <sachanhardik@gmail.com>
2024-05-29 04:38:31 +0530

    add tmux-vim navigator and set neotree bg to transparent

--- lua/theeditor/lazy/colors.lua
+++ lua/theeditor/lazy/colors.lua
@@ -4,6 +4,8 @@ function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
 
 end
 
--- lua/theeditor/lazy/lsp.lua
+++ lua/theeditor/lazy/lsp.lua
@@ -70,8 +70,8 @@ return {
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-                ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
                 ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
